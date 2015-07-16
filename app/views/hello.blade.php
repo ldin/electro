70f732cf37b47d5c52c9e835a3cad3b38a89e6d8
Alena Blokhina <a.blokhina@dlinksys.ru>
2015-07-16 10:55:29 +0300

    метрика

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -410,7 +410,50 @@
 			</div>
 		</div>
 	</footer>
+
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter31454463 = new Ya.Metrika({
+                    id:31454463,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/31454463" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<!-- BEGIN JIVOSITE CODE {literal} -->
+
+<script type='text/javascript'>
+
+(function(){ var widget_id = '5NDzybHSuF';
+
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+
+<!-- {/literal} END JIVOSITE CODE -->
+
 </body>
+
 </html>
 
 ­
